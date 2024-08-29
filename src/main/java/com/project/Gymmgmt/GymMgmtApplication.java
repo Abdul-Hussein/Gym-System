134499a8865736d54ee972575a6ac8040cc027c5
Abdul-Hussein <136578029+Abdul-Hussein@users.noreply.github.com>
2024-08-29 14:23:29 +0300

    commit

--- src/main/java/com/project/Gymmgmt/GymMgmtApplication.java
+++ src/main/java/com/project/Gymmgmt/GymMgmtApplication.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication // @Configuration,@EnableAutoConfiguration and @ComponentScan annotations
 public class GymMgmtApplication extends SpringBootServletInitializer {
 
+	
+	@GetMapping("deploy")
+	public String message() {
+		return "Deploy successful";
+	}
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(GymMgmtApplication.class);
