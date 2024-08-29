338e8dea620b502d10dc11d7fbc537b420b906f8
Abdul-Hussein <136578029+Abdul-Hussein@users.noreply.github.com>
2024-08-29 12:04:55 +0300

    Deployment test

--- src/main/java/com/project/Gymmgmt/GymMgmtApplication.java
+++ src/main/java/com/project/Gymmgmt/GymMgmtApplication.java
@@ -4,14 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication // @Configuration,@EnableAutoConfiguration and @ComponentScan annotations
 public class GymMgmtApplication extends SpringBootServletInitializer {
 
+	@GetMapping("/success")
+	public String Message() {
+		return "Deployment successful";
+	}
+	
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(GymMgmtApplication.class);
 	}
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(GymMgmtApplication.class, args);
